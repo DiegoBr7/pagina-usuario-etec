f4a53f1cb30c25fa4b4351bec0901a5bc389e5f7
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2024-06-23 20:49:51 -0300

    pagina usuario

---
+++ script.js
@@ -0,0 +1,169 @@
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4, // Número de cartões visíveis para telas grandes
+//   spaceBetween: 30,
+//   slidesPerGroup: 4, // Número de cartões movidos ao navegar
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   breakpoints: {
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
+//       slidesPerGroup: 4,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//       slidesPerGroup: 3,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//       slidesPerGroup: 2,
+//     },
+//     480: {
+//       slidesPerView: 1,
+//       spaceBetween: 10,
+//       slidesPerGroup: 1,
+//     },
+//   },
+// });
+
+
+// Inicialização do Swiper para Lojas
+var swiperLojas = new Swiper(".mySwiperLojas", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+      el: ".swiper-pagination-lojas",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next-lojas",
+      prevEl: ".swiper-button-prev-lojas",
+  },
+  breakpoints: {
+      1200: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+          slidesPerGroup: 4,
+      },
+      992: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+      },
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          slidesPerGroup: 2,
+      },
+      480: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+          slidesPerGroup: 1,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+    },
+  },
+});
+
+// Inicialização do Swiper para Clínicas
+var swiperClinicas = new Swiper(".mySwiperClinicas", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+      el: ".swiper-pagination-clinicas",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next-clinicas",
+      prevEl: ".swiper-button-prev-clinicas",
+  },
+  breakpoints: {
+      1200: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+          slidesPerGroup: 4,
+      },
+      992: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+      },
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          slidesPerGroup: 2,
+      },
+      480: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+          slidesPerGroup: 1,
+      },
+      320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+          slidesPerGroup: 1,
+      },
+  },
+});
+
+
+
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4, // Número de cartões visíveis para telas grandes
+//   spaceBetween: 30,
+//   slidesPerGroup: 4, // Número de cartões movidos ao navegar
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   breakpoints: {
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
+//       slidesPerGroup: 4,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//       slidesPerGroup: 3,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//       slidesPerGroup: 2,
+//     },
+//     480: {
+//       slidesPerView: 1,
+//       spaceBetween: 10,
+//       slidesPerGroup: 1,
+//     },
+//   },
+// });
